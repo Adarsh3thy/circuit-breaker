3e4267b14f2863c1350da12ecb0e83069e1dd1b0
Sandesh Gupta <sandesh.gupta93@gmail.com>
2021-12-01 00:25:26 -0800

    Added endpoint for timeout request, Added loggers

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 from flask import Flask,request,redirect,Response
-import requests
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+TIMEOUT_SECONDS = 5
 
 app = Flask(__name__)
 
@@ -13,7 +18,14 @@ def hello():
 
 @app.route('/get_response/<responseType>',methods=['GET','POST','DELETE'])
 def get_response(responseType):
+    logging.info('Request response {}'.format(responseType))
     return 'Returning response', responseType
+
+@app.route('/timeout/',methods=['GET'])
+def timeout():
+    logging.info('Sleeping for {} seconds'.format(TIMEOUT_SECONDS))
+    time.sleep(TIMEOUT_SECONDS)
+    return 'Timeout response', 200
 
 if __name__ == '__main__':
     app.run(debug = False,port=8080)
