18e80e165b679010e542cb1b6f16c282bf4f2c9e
Rooppesh <rooppesh.sarankapani@gmail.com>
2021-11-30 23:36:44 -0800

    refactoring

--- proxy.py
+++ proxy.py
@@ -1,14 +1,44 @@
-from twisted.web import proxy, http
-from twisted.internet import reactor
-from twisted.python import log
-import sys
+from flask import Flask,request,redirect,Response
+import requests
+from circuit_breaker import CircuitBreaker
 
-log.startLogging(sys.stdout)
+app = Flask(__name__)
 
+SITE_NAME = 'http://localhost:8080/'
 
-class ProxyFactory(http.HTTPFactory):
-    protocol = proxy.Proxy
+@app.route('/')
+def index():
+    return 'This is proxy!'
 
+@app.route('/checkCircuitBreakerHealth')
+def check_circuit_breaker_health():
+    return CircuitBreaker.print_health(self)
 
-reactor.listenTCP(8080, ProxyFactory())
-reactor.run()
+@CircuitBreaker
+def proxy_request(path):
+    print("process request on server()")
+    global SITE_NAME
+
+    if request.method=='GET':
+        resp = requests.get(f'{SITE_NAME}{path}')
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+    elif request.method=='POST':
+        resp = requests.post(f'{SITE_NAME}{path}',json=request.get_json())
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+    elif request.method=='DELETE':
+        resp = requests.delete(f'{SITE_NAME}{path}')
+        response = Response(resp.content, resp.status_code)
+        return response
+
+@app.route('/<path:path>',methods=['GET','POST','DELETE'])
+def proxy(path):
+    return proxy_request(path)
+
+if __name__ == '__main__':
+    app.run(debug = False,port=82)
