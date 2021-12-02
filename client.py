fff8fc772b4acb82f667b50d854b0747be3371d7
Anuhya Gankidi <gankidianuhya@gmail.com>
2021-12-02 14:38:11 -0800

    timeout cases

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 import time
 
+
 SERVER_URL = "http://localhost:1082"
 
 
@@ -91,3 +92,75 @@ requests.get(SERVER_URL+"/get_response/500")
 
 print_circuit_breaker_health("End")
 requests.post(SERVER_URL+"/resetCircuitBreaker")
+
+
+print("----------------------------------------------")
+print("Test Case 6: Close to Close with TIMEOUT")
+print_circuit_breaker_health("Start")
+
+requests.get(SERVER_URL+"/get_response/200")
+requests.get(SERVER_URL+"/get_response/200")
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+
+print_circuit_breaker_health("End")
+requests.post(SERVER_URL+"/resetCircuitBreaker")
+
+
+print("----------------------------------------------")
+print("Test Case 7: Close to Open when TIMEOUT")
+print_circuit_breaker_health("Start")
+
+requests.get(SERVER_URL+"/get_response/200")
+requests.get(SERVER_URL+"/get_response/200")
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+
+print_circuit_breaker_health("End")
+requests.post(SERVER_URL+"/resetCircuitBreaker")
+
+
+print("----------------------------------------------")
+print("Test Case 8: Open to HalfOpen to Open (After Time Period) when TIMEOUT")
+print_circuit_breaker_health("Start")
+
+time.sleep(35)
+
+requests.get(SERVER_URL+"/timeout")
+
+print_circuit_breaker_health("End")
+requests.post(SERVER_URL+"/resetCircuitBreaker")
+
+print("----------------------------------------------")
+print("Test Case 9: Open to Half-Open to Close (After Time Period) with TIMEOUT")
+
+move_circuit_breaker_to_open()
+print_circuit_breaker_health("Start")
+
+time.sleep(35)
+
+requests.get(SERVER_URL+"/get_response/200")
+requests.get(SERVER_URL+"/timeout")
+
+print_circuit_breaker_health("End")
+requests.post(SERVER_URL+"/resetCircuitBreaker")
+
+
+print("----------------------------------------------")
+print("Test Case 10: Open to Open with TIMEOUT")
+
+move_circuit_breaker_to_open()
+print_circuit_breaker_health("Start")
+
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+requests.get(SERVER_URL+"/timeout")
+
+print_circuit_breaker_health("End")
+requests.post(SERVER_URL+"/resetCircuitBreaker")
+
