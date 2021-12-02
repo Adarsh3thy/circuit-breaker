4e12335de8a37f87293353c9ff181e13cd2f79c7
Sandesh Gupta <sandesh.gupta93@gmail.com>
2021-12-02 00:38:28 -0800

    Decode Response status instead of Redis

--- proxy.py
+++ proxy.py
@@ -46,19 +46,16 @@ def proxy_request(path):
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
-        redis.set('status',resp.status_code)
         return response
     elif request.method=='POST':
         resp = requests.post(f'{SITE_NAME}{path}',json=request.get_json())
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
-        redis.set('status', resp.status_code)
         return response
     elif request.method=='DELETE':
         resp = requests.delete(f'{SITE_NAME}{path}')
         response = Response(resp.content, resp.status_code)
-        redis.set('status', resp.status_code)
         return response
 
 @app.route('/<path:path>',methods=['GET','POST','DELETE'])
