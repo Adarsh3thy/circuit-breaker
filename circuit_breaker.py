d25f76d93131ca4171a51ed95df5cdd89806881e
Adarsh Murthy <adarsh.narasimhamurthy@sjsu.edu>
2021-12-02 10:59:30 -0800

    Removing unused imports

--- circuit_breaker.py
+++ circuit_breaker.py
@@ -1,27 +1,27 @@
 from enum import Enum
 from datetime import datetime, timedelta
 from flask import Response
-from flask.json import jsonify
 import logging
 import redis
-import sys
 
 logging.basicConfig(level=logging.INFO)
 
 redis = redis.StrictRedis(
     host='localhost',
-    port='6379',decode_responses=True)
+    port='6379', decode_responses=True)
+
 
 class CIRCUIT_BREAKER_STATES(Enum):
     OPEN = 1
     CLOSED = 2
     HALF_OPEN = 3
 
+
 class CircuitBreaker(object):
     FAILURE_COUNT_THRESHOLD = 3
     RECOVERY_TIME = 30
     FAILURE_WINDOW = 600
-    EXCEPTION_TO_FAIL_FOR = Exception 
+    EXCEPTION_TO_FAIL_FOR = Exception
 
     def __init__(self, f, _failure_count_threshold=None,
                  exception_to_fail_for=None):
@@ -43,7 +43,8 @@ class CircuitBreaker(object):
 
         print("Current State of CB {}", CIRCUIT_BREAKER_STATES(self.check_state()).name)
         if self.check_state() == CIRCUIT_BREAKER_STATES.OPEN.value:
-            current_timelapse = datetime.utcnow() - datetime.strptime(redis.get('_circuit_open_time'),'%B %d %Y - %H:%M:%S')
+            current_timelapse = datetime.utcnow() - datetime.strptime(redis.get('_circuit_open_time'),
+                                                                      '%B %d %Y - %H:%M:%S')
             print("Open state, time elapsed: {}", current_timelapse)
             if current_timelapse.total_seconds() < CircuitBreaker.RECOVERY_TIME:
                 ret_val = Response("The server is currently unavailable, please try after sometime", 503)
@@ -85,8 +86,9 @@ class CircuitBreaker(object):
         self._failure_count += 1
         redis.set('_failure_count', self._failure_count)
 
-        self._failure_window_start_time = datetime.strptime(redis.get('_failure_window_start_time'),'%B %d %Y - %H:%M:%S')
-        failure_time_obj=datetime.strptime(redis.get('_failure_window_time'), "%H:%M:%S") - datetime(1900, 1, 1)
+        self._failure_window_start_time = datetime.strptime(redis.get('_failure_window_start_time'),
+                                                            '%B %d %Y - %H:%M:%S')
+        failure_time_obj = datetime.strptime(redis.get('_failure_window_time'), "%H:%M:%S") - datetime(1900, 1, 1)
         self._failure_window_time = timedelta(seconds=failure_time_obj.total_seconds())
         if (self._failure_window_start_time + self._failure_window_time) < datetime.utcnow():
             self.reset_counters()
@@ -129,13 +131,17 @@ class CircuitBreaker(object):
 """
 Examples to test the decorator and CB
 """
+
+
 @CircuitBreaker
 def server_call(path):
     print("process request on server()")
 
+
 @CircuitBreaker
 def server_call_2():
     print("process request on server_2()")
+
 
 server_call('aa')
 # server_call_2()
